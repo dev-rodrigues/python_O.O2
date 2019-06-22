c62b511f358f38b1f79285de7def911a01dff62b
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-06-21 22:03:52 -0300

    refatorando codigo

--- modelo.py
+++ modelo.py
@@ -1,61 +1,45 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
 
-## filme ##
-class Filme:
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+## filme ## - filha
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
-## serie ##
-class Serie:
+## serie ## filha
+class Serie(Programa):
     def __init__(self, nome, ano, temporada):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporada = temporada
-        self.__likes = 0
 
-    def dar_like(self):
-        self.likes += 1
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-
+##teste
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
       f' - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
+##teste
 atlanta = Serie('atlanta', 2017, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.nome} '
+print(f'Nome: {atlanta.nome} - Ano: {atlanta._nome} '
       f' - Duração: {atlanta.temporada} - Likes: {atlanta.likes}')
